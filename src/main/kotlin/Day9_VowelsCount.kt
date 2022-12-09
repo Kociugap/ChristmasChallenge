acf93b97d1133b1b4c928579c9521c089f7b64a7
Paweł Kociuga <pawel.kociuga@j-labs.pl>
2022-12-09 09:16:56 +0100

    Day9_VowelsCount

---
+++ src/main/kotlin/Day9_VowelsCount.kt
@@ -0,0 +1,57 @@
+package main.kotlin
+
+fun main() {
+    val vowels = setOf('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'y', 'Y', 'u', 'U')
+    val remaining = prepareAddresses().joinToString("") { it.street }.count { char -> char in vowels }
+    println(remaining * 2)
+}
+
+private fun prepareAddresses(): List<Address> {
+    val addresses = mutableListOf<Address>()
+    addresses.add(
+        Address(
+            "Serniczkowa",
+            "4B",
+            2,
+            "Krakow",
+            "02-326"
+        )
+    )
+    addresses.add(
+        Address(
+            "Pierniczkowa",
+            "289A",
+            55,
+            "Gdansk",
+            "02-326"
+        )
+    )
+    addresses.add(
+        Address(
+            "Barszczykowa",
+            "234",
+            5,
+            "Rzeszow",
+            "37-326"
+        )
+    )
+    addresses.add(
+        Address(
+            "Uszkowa",
+            "43H",
+            5,
+            "Wroclaw",
+            "02-326"
+        )
+    )
+    addresses.add(
+        Address(
+            "Cukierkowa",
+            "23I",
+            5,
+            "Poznan",
+            "02-326"
+        )
+    )
+    return addresses
+}
